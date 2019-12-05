f1981e64f3d8e8c3074d09de5bb633aa20ddf014
htr-volker <htr.volker@gmail.com>
2019-12-05 22:02:18 +0000

    fixes fixes

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -118,10 +118,10 @@ def enter_observation():
         # Create an association with the observers and the observation
         if form.observer1.data != None:
             observer1_id = Users.query.filter_by(user_name = form.observer1.data).first()
-            observation_data.observers.append(observer1_id.userID)
+            observation_data.observers.append(observer1_id)
         if form.observer2.data != None:
             observer2_id = Users.query.filter_by(user_name = form.observer2.data).first()
-            observation_data.observers.append(observer2_id.userID)
+            observation_data.observers.append(observer2_id)
 
         # Add these changes to the database and commit
         db.session.add(observation_data)
