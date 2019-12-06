b1764b5f89b369ef9eb335ff08257c898395d681
htr-volker <htr.volker@gmail.com>
2019-12-06 10:42:25 +0000

    delete account button now should delete observations posted by account

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -102,6 +102,7 @@ def account():
         form.email.data = current_user.email
     
     if delete_account.validate_on_submit:
+        Observations.query.filter_by(userID = current_user.userID).delete()
         Users.query.filter_by(userID = current_user.userID).delete()
         db.session.commit()
 
