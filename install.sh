9d18d37fed88dbf610fe1d863e02cb7cd2cf819b
htr-volker <57865118+htr-volker@users.noreply.github.com>
2019-12-06 15:31:45 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+# install the service script
+sudo cp flask-app.service /etc/systemd/system/
+# reload the service scripts
+sudo systemctl daemon-reload
+# stop the old service
+sudo systemctl stop flask-app
+# install the application files
+install_dir=/opt/flask-app
+sudo rm -rf ${install_dir}
+sudo mkdir ${install_dir}
+sudo cp -r ./* ${install_dir}
+sudo chown -R pythonadm:pythonadm ${install_dir}
+# configure python virtual environment and install dependencies
+sudo su - pythonadm << EOF
+cd ${install_dir}
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+EOF
